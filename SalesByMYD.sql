729927ff0cf0a2311787b9c0af2f00837d4d6467
Aidan Stone <102496024+aidanstone25@users.noreply.github.com>
2022-08-29 13:29:11 -0400

    Delete SalesByMYD.sql

--- SalesByMYD.sql
+++
@@ -1,8 +0,0 @@
-SELECT * FROM salesorderdetail;
-SELECT * FROM salesorderheader;
-
-
-SELECT salesorderdetail.ModifiedDate,SUM(salesorderdetail.LineTotal) FROM salesorderdetail GROUP BY YEAR(salesorderdetail.ModifiedDate);
-SELECT salesorderdetail.ModifiedDate,SUM(salesorderdetail.LineTotal) FROM salesorderdetail GROUP BY DAY(salesorderdetail.ModifiedDate);
-SELECT salesorderdetail.ModifiedDate,SUM(salesorderdetail.LineTotal) FROM salesorderdetail GROUP BY MONTH(salesorderdetail.ModifiedDate);
-
